62acfbd2be6be8474a7fbd741d0a67e637772ff7
Emir <66549233+Memirdogan@users.noreply.github.com>
2023-12-24 10:49:28 +0300

    Time: 1278 ms (48.75%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT name FROM SalesPerson
+WHERE sales_id NOT IN (
+  SELECT sales_id FROM Orders AS O
+  LEFT JOIN Company AS C ON O.com_id = C.com_id
+  WHERE name = 'RED'
+)
