1c8ed3ec32e97eaf2c578a668bd07dec46966f71
Emir <66549233+Memirdogan@users.noreply.github.com>
2024-03-15 20:42:56 +0300

    Time: 1715 ms (18.44%), Space: 17.5 MB (67.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,6 +3,6 @@ class Solution:
         n = len(nums)
         for i in range(n - 1):
             for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
+                if nums[i] + nums[j] ==  target:
                     return [i, j]
         return []  # No solution found
